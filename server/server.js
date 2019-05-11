d440013462e179a72dff7346f8824aa36aa48072
Kumar Shashwat <KSSBro@users.noreply.github.com>
2019-05-11 22:07:06 +0530

    Changed quesGetRoute.js and quesPostRoute.js to getRoute.js and postRoute.js respectively | Changed imports in server.js | Updated checklist

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 //Importing modules
-const express = require('express');
-const hbs = require('hbs');
-const path = require('path');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const morganLogger = require('morgan');
-const crypto = require('crypto')
+const express = require("express");
+const hbs = require("hbs");
+const path = require("path");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const morganLogger = require("morgan");
+const crypto = require("crypto");
 //Importing modules
 
 /*The constant port will be set to 108 for localhost and for deployment it will be set to the port 
@@ -15,44 +15,48 @@ const port = process.env.PORT || 108;
 const server = express();
 
 //Database connection and models
-const { mongoose } = require('./db/mongoose.js');
-const { User } = require('./models/user.js');
-const { Question } = require('./models/question.js');
+const { mongoose } = require("./db/mongoose.js");
+const { User } = require("./models/user.js");
+const { Question } = require("./models/question.js");
 
-//Importing the route files 
-const authRoute = require('./routes/authRoute.js');
-const pageRoute = require('./routes/pageRoute.js');
-const quesPostRoute = require('./routes/quesPostRoute.js');
-const quesGetRoute = require('./routes/quesGetRoute.js');
+//Importing the route files
+const authRoute = require("./routes/authRoute.js");
+const pageRoute = require("./routes/pageRoute.js");
+const postRoute = require("./routes/postRoute.js");
+const getRoute = require("./routes/getRoute.js");
 
 //Middlewares
-const { sessionChecker }  = require('./middleware/sessionChecker.js');
+const { sessionChecker } = require("./middleware/sessionChecker.js");
 
-server.use(express.static(path.join(__dirname, '../public')));
-server.use(bodyParser.urlencoded({
- extended: false 
-}));
+server.use(express.static(path.join(__dirname, "../public")));
+server.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 server.use(bodyParser.json());
 server.use(cookieParser());
-server.use(morganLogger('dev'));
+server.use(morganLogger("dev"));
 
 //Setting up the session and cookie
-server.use(session({
-	key: 'user_sid',
-	secret: crypto.randomBytes(20).toString('hex'),
-	resave: false,
-	saveUninitialized: false,
-	cookie: {
-		expires: 600000
-	}
-}));
+server.use(
+  session({
+    key: "user_sid",
+    secret: crypto.randomBytes(20).toString("hex"),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 600000
+    }
+  })
+);
 
 //Deleting the cookie if the cookie is sent but the user does not exist in the session
 server.use((req, res, next) => {
-	if(req.cookies.user_sid && !req.session.user) {
-		res.clearCookie('user_sid');
-	}
-	next();
+  if (req.cookies.user_sid && !req.session.user) {
+    res.clearCookie("user_sid");
+  }
+  next();
 });
 
 //Using the routers from the route files
@@ -65,22 +69,22 @@ server.use(authRoute.router);
 server.use(pageRoute.router);
 // Route: Pages
 
-// Route: Question(POST)
-server.use(quesPostRoute.router);
-// Route: Question(POST)
+// Route: Post Route
+server.use(postRoute.router);
+// Route: Post Route
 
-// Route: Question(GET)
-server.use(quesGetRoute.router);
-// Route: Question(GET)
+// Route: Get Route
+server.use(getRoute.router);
+// Route: Get Route
 
 // 404 Page Not Found
-server.use(function(req, res, next){
-    res.status(404).render('pagenotfound.hbs', {
-    	data: "Sorry, page not found"
-    });
+server.use(function(req, res, next) {
+  res.status(404).render("pagenotfound.hbs", {
+    data: "Sorry, page not found"
+  });
 });
 // 404 Page Not Found
 
 server.listen(port, () => {
-	console.log(`App running on ${port}`);
+  console.log(`App running on ${port}`);
 });
